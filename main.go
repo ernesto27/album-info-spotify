553c782efdab92c12abd3e88963472ad5e88407f
ernesto <ernestoponce27@gmail.com>
2020-11-07 18:54:00 -0300

    Add label , review data ,  get info for current playing spotify app

--- main.go
+++ main.go
@@ -1,33 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 )
 
 type Album struct {
+	Artist      string `json:"strArtist"`
+	Name        string `json:"strAlbum"`
 	Description string `json:"strDescriptionEN"`
 	ReleaseYear string `json:"intYearReleased"`
 	Style       string `json:"strStyle"`
+	Review      string `json:"strReview"`
+	Label       string `json:"strLabel"`
 }
 
 type ResponseAlbum struct {
 	Album []Album
 }
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("You have to pass bandName and albumName arguments")
-		fmt.Println("$ go run main.go megadeth 'rust in peace' ")
-		os.Exit(1)
-	}
+var nameBand string
+var albumBand string
 
-	nameBand := strings.ReplaceAll(os.Args[1], " ", "%20")
-	albumBand := strings.ReplaceAll(os.Args[2], " ", "%20")
+func main() {
+
+	if len(os.Args) < 3 {
+		// Check if python is available
+		// CHeck error python
+
+		// If no params passed check current spotify album playing
+		cmd := exec.Command("python", "getAlbumSpotify.py")
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			panic(err)
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+		err = cmd.Start()
+		if err != nil {
+			panic(err)
+		}
+
+		go copyOutput(stdout)
+		go copyOutput(stderr)
+		cmd.Wait()
+
+		nameBand = strings.ReplaceAll(nameBand, " ", "%20")
+		albumBand = strings.ReplaceAll(albumBand, " ", "%20")
+
+		// fmt.Println("You have to pass bandName and albumName arguments")
+		// fmt.Println("$ go run main.go megadeth 'rust in peace' ")
+		// os.Exit(1)
+	} else {
+		// Search by band , album enter by user
+		nameBand = os.Args[1]
+		albumBand = os.Args[2]
+		nameBand = strings.ReplaceAll(nameBand, " ", "%20")
+		albumBand = strings.ReplaceAll(albumBand, " ", "%20")
+	}
 
 	album := new(ResponseAlbum)
 	getJson("https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s="+nameBand+"&a="+albumBand, album)
@@ -37,10 +76,12 @@ func main() {
 	}
 
 	fmt.Println()
-	fmt.Println(os.Args[1] + " - " + os.Args[2])
+	fmt.Println(album.Album[0].Artist + " - " + album.Album[0].Name)
 	fmt.Println(album.Album[0].ReleaseYear + " - " + album.Album[0].Style)
+	if album.Album[0].Label != "" {
+		fmt.Println("Label: " + album.Album[0].Label)
+	}
 	fmt.Println()
-	fmt.Println("----------------------------------------------------------------------------------------------------------------------------------------")
 	fmt.Println("----------------------------------------------------------------------------------------------------------------------------------------")
 	fmt.Println()
 
@@ -49,6 +90,14 @@ func main() {
 
 	fmt.Println()
 	fmt.Println("----------------------------------------------------------------------------------------------------------------------------------------")
+	fmt.Println()
+
+	if album.Album[0].Review != "" {
+		fmt.Println("REVIEW:")
+		fmt.Println(string(album.Album[0].Review))
+	}
+
+	fmt.Println()
 	fmt.Println("----------------------------------------------------------------------------------------------------------------------------------------")
 
 }
@@ -63,4 +112,17 @@ func getJson(url string, target interface{}) error {
 	defer r.Body.Close()
 
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func copyOutput(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	index := 0
+	for scanner.Scan() {
+		if index == 0 {
+			nameBand = scanner.Text()
+		} else if index == 1 {
+			albumBand = scanner.Text()
+		}
+		index += 1
+	}
 }
